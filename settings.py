bf3b24ab409b190913137baa0c029fc710868414
Gaofeng Shen <shengaofeng@gmail.com>
2015-03-02 21:58:19 +0800

    新文件：    settings.py 	修改：      auth.py 	修改：      initdb.py 	修改：      main.py 	修改：      updatetoken.py 	修改：      wechathandlers.py

---
+++ settings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#coding:utf8
+
+DATABASE = {
+    "host":"localhost",
+    "port":"3306",
+    "database":"test",
+    "username":"test",
+    "password":"test",
+    }
+
+MEDIA_DIR = "~/temp/media_file"
+
+SECRET = ""
+APPID = ""
+EncodingAESKey = ""
