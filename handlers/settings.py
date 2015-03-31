cfebae024ef149394975dbb90c4b87252a617d7c
Gaofeng Shen <shengaofeng@gmail.com>
2015-03-31 21:26:48 +0800

    修改：      settings.py

--- handlers/settings.py
+++ handlers/settings.py
@@ -5,8 +5,8 @@ DATABASE = {
     "host": "localhost",
     "port": "3306",
     "database": "test",
-    "username": "root",
-    "password": "236788",
+    "username": "test",
+    "password": "test",
     }
 
 MEDIA_DIR = "~/temp/media_file"
